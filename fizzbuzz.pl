09faff26ef81d346061407ffbd951f3aa2bf8681
Tri Nguyen <mail@trimn.net>
2015-04-18 05:17:57 +0200

    Update perl fizzbuzz

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -1,7 +1,9 @@
 #!/usr/bin/env perl
+use feature qw(say);
+
 for my $i (1..100) {
-  print $i % 15 == 0 ? "FizzBuzz "
-    : $i % 3 == 0 ? "Fizz "
-    : $i % 5 == 0 ? "Buzz "
-    : $i . " "
+  say $i % 15 == 0 ? "FizzBuzz"
+    : $i % 3 == 0 ? "Fizz"
+    : $i % 5 == 0 ? "Buzz"
+    : $i
 }
