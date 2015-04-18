e7e794a4eea2b0208ae353f3ec70b1828332697b
Tri Nguyen <mail@trimn.net>
2015-04-18 05:21:44 +0200

    Update js fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,23 +1,8 @@
-/**
- * This is a JavaScript implementation for FizzBuzz for the first 100 positive integers
- * It relies on Node.js process.stdout since JavaScript doesn't have a standard I/O library
- * Run with `node fizzbuzz.js`
- */
-
-for(i = 1; i <= 100; i++) {
-	var s = "";
-
-	if(i % 3 === 0) {
-		s += "Fizz ";
-	}
-
-	if(i % 5 === 0) {
-		s += "Buzz ";
-	}
-
-	if(s === "") {
-		s = i + " ";
-	}
-
-	process.stdout.write(s);
+for (i = 1; i <= 100; i++) {
+	console.log(
+		i % 15 === 0 ? 'FizzBuzz'
+		: i % 3 === 0 ? 'Fizz'
+		: i % 5 === 0 ? 'Buzz'
+		: i
+	);
 }
